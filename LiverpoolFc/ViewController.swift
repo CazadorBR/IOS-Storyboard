cdc40e9d68f95d66130ea80ffb8aebcfed5c845c
Ben Romdhane Fedi <fedi.benromdhane@gmail.com>
2023-12-30 12:24:31 +0100

    Initial Commit

---
+++ LiverpoolFc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiverpoolFc
+//
+//  Created by ben romdhane fedi on 2023-12-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
