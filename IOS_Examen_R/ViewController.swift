3f6ad6d429ea09490185fb03682964ce8e96ba11
Ben Romdhane Fedi <fedi.benromdhane@gmail.com>
2024-01-15 08:45:36 +0100

    Initial Commit

---
+++ IOS_Examen_R/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_Examen_R
+//
+//  Created by ben romdhane fedi on 2024-01-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
